0c041a517ea62974b3d4b5e1d3adc8a11c993056
lgomdom0202 <lgomdom0202@g.educaand.es>
2025-02-12 01:28:57 +0100

    Solucionado el Main, ahora está mejor

--- Departamento.kt
+++ Departamento.kt
@@ -2,27 +2,33 @@ class Departamento(
     val empleados: MutableList<Empleado>
 ) {
     fun agregarEmpleado(){
-        val nombre = Consola.pedirDato("el nombre del empleado")
-        val id = Consola.pedirDato("el id del empleado")
-        val tipo = Consola.tipoEmpleado()
-        var empleado:Empleado? = null
-        if(tipo == "1"){
-            empleado = EmpleadoFijo(nombre,id,Consola.pedirDecimal("el salario fijo del empleado"),Consola.pedirEntero("el numero de pagas"))
-        }
-        else{
-            empleado = EmpleadoPorHora(nombre,id,Consola.pedirEntero("el numero de horas trabajadas del empleado"),Consola.pedirDecimal("la tarifa por hora del empleado"))
-        }
+
+
+    val nombre = Consola.pedirDato("el nombre del empleado")
+        var id = ""
+        do {
+                id = Consola.pedirDato("el id del empleado (debe medir 4 caracteres)")
+                if (id.length != 4 || !comprobarid(id)){
+                    println("ERROR, VUELVE A INTRODUCIR, DEBE MEDIR 4 CARACTERES Y SER TODOS NUMÉRICOS")
+                }
+            }while(id.trim().length != 4)
+
+            val tipo = Consola.tipoEmpleado()
+            var empleado:Empleado? = null
+            if(tipo == "1"){
+                empleado = EmpleadoFijo(nombre,id,Consola.pedirDecimal("el salario fijo del empleado"),Consola.pedirEntero("el numero de pagas"))
+            }
+            else{
+                empleado = EmpleadoPorHora(nombre,id,Consola.pedirEntero("el numero de horas trabajadas del empleado"),Consola.pedirDecimal("la tarifa por hora del empleado"))
+            }
         empleados.add(empleado)
     }
 
-
-
-
-
-    fun calcularSalarioTotal(){
+    fun calcularSalarioTotal():Double{
         var salarioTotal = 0.0
         for(empleado in empleados){
             salarioTotal+= empleado.calculaSalario()
         }
+        return salarioTotal
     }
 }
--- Main.kt
+++ Main.kt
@@ -12,35 +12,16 @@ fun comprobarid(id:String):Boolean{
 
 fun main(){
     val listaEmpleado = mutableListOf<Empleado>()
-    var salida = false
-    var id = ""
-    do{
-        val nombre = Consola.pedirDato("el nombre del empleado")
-        do {
-            id = Consola.pedirDato("el id del empleado (debe medir 4 caracteres)")
-            if (id.length != 4 || !comprobarid(id)){
-                println("ERROR, VUELVE A INTRODUCIR, DEBE MEDIR 4 CARACTERES Y SER TODOS NUMÉRICOS")
-            }
-        }while(id.length != 4)
-
-        val tipo = Consola.tipoEmpleado()
-        var empleado:Empleado? = null
-
-        if(tipo == "1"){
-            empleado = EmpleadoFijo(nombre,id,Consola.pedirDecimal("el salario fijo del empleado"),Consola.pedirEntero("el numero de pagas"))
-        }
-        else{
-            empleado = EmpleadoPorHora(nombre,id,Consola.pedirEntero("el numero de horas trabajadas del empleado"),Consola.pedirDecimal("la tarifa por hora del empleado"))
-        }
-        listaEmpleado.add(empleado)
-
-        salida = Consola.salir()
-
-    }while(!salida)
-
     val departamento = Departamento(listaEmpleado)
+    var salida = false
+    do{
+        departamento.agregarEmpleado()
+        salida = Consola.salir()
+    }while(!salida)
 
     for(empleado in departamento.empleados){
         println("${empleado.nombre} con ${"%04d".format(empleado.id.toInt())} tiene un salario de ${"%.2f".format(empleado.calculaSalario())} al mes.")
     }
+
+    println("El salario total es de ${departamento.calcularSalarioTotal()}")
 }
